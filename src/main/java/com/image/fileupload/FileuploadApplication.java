9efbfe3c0955f9e17e198194d665de4ccbc5b18c
Lee JaeHyeok <dhfif718@gmail.com>
2023-01-15 21:41:04 +0900

    [fix] 불필요 제거

--- src/main/java/com/image/fileupload/FileuploadApplication.java
+++ src/main/java/com/image/fileupload/FileuploadApplication.java
@@ -9,14 +9,14 @@ import java.util.TimeZone;
 @SpringBootApplication
 public class FileuploadApplication {
 
-	@PostConstruct
-	public void started(){
-		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-	}
-
-	static {
-		System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
-	}
+//	@PostConstruct
+//	public void started(){
+//		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+//	}
+//
+//	static {
+//		System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FileuploadApplication.class, args);
